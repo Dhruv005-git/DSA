567bf1e0353530071789d1210275c9335cabbe69
Dhruv Jain <jaindhru005@gmail.com>
2026-07-26 23:09:53 +0530

    Time: 766 ms (5.02%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,22 +1,13 @@
+# Write your MySQL query statement below
 SELECT
-    id,
     CASE
         WHEN id = (SELECT MAX(id) FROM Seat)
              AND id % 2 = 1
-        THEN student
-
+        THEN id
         WHEN id % 2 = 1
-        THEN (
-            SELECT student
-            FROM Seat s2
-            WHERE s2.id = Seat.id + 1
-        )
-
-        ELSE (
-            SELECT student
-            FROM Seat s2
-            WHERE s2.id = Seat.id - 1
-        )
-    END AS student
+        THEN id + 1
+        ELSE id - 1
+    END AS id,
+    student
 FROM Seat
 ORDER BY id;
