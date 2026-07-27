90704a6a08bc9de1ac91d4f5f4d17e031ec494e0
Dhruv Jain <jaindhru005@gmail.com>
2026-07-27 23:57:18 +0530

    Time: 319 ms (20.39%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,13 +1,2 @@
 # Write your MySQL query statement below
-SELECT (
-    SELECT salary
-    FROM (
-        SELECT salary,
-               ROW_NUMBER() OVER (ORDER BY salary DESC) AS rn
-        FROM (
-            SELECT DISTINCT salary
-            FROM Employee
-        ) d
-    ) t
-    WHERE rn = 2
-) AS SecondHighestSalary;
+select max(salary) as SecondHighestSalary from Employee where salary not in (select max(salary) from Employee);
